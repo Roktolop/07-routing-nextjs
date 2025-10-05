a25b38f09b7c53ca94a753f2316095da5b5bea72
Volodimir <sanin.vova200@gmail.com>
2025-10-05 13:27:02 +0300

    Refactor NotePreview components: update NotePreviewClient to use useQuery for data fetching and integrate hydration

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,13 +1,25 @@
 'use client';
 
-import { Note } from '@/types/note';
+import { useParams } from 'next/navigation';
 import css from './NotePreview.module.css'
+import { fetchNoteById } from '@/lib/api';
+import { useQuery } from '@tanstack/react-query';
 
-interface Props {
-  note: Note
-}
+export function NotePreviewClient() {
+  const { id } = useParams<{ id: string }>();
 
-export function NotePreviewClient({ note }: Props) {
+  const { data: note, isLoading, error } = useQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
+  })
+
+  console.log(note);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error || !note) return <p>Some error..</p>;
+
   return (
     <>
       <div className={css.container}>
--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,21 +1,31 @@
 import { fetchNoteById } from '@/lib/api';
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Modal } from '@/components/Modal/Modal';
 import { NotePreviewClient } from './NotePreview.client';
+import { QueryClient } from '@tanstack/react-query';
 
 type Props = {
   params: Promise<{ id: string }>;
 };
 
 export default async function NotePreview({ params }: Props) {
+  const queryClient = new QueryClient();
+
   const { id } = await params;
-  const note = await fetchNoteById(id);
-  console.log(note);
+
+  await queryClient.prefetchQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+  })
+
+  const dehydratedState = dehydrate(queryClient)
 
   return (
     <Modal>
-      <NotePreviewClient note={note}></NotePreviewClient>
+      <HydrationBoundary state={dehydratedState}>
+        <NotePreviewClient></NotePreviewClient>
+      </HydrationBoundary>
     </Modal>
-
   );
 };
 
